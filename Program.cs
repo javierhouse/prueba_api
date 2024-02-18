1ddb169cf8e55b579546f7e4f816e53b2199dfc7
javierhouse <j.debenito@hotmail.com>
2024-02-18 18:37:21 +0100

    # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # # On branch main # Your branch is up to date with 'origin/main'. # # Changes to be committed: #	modified:   Program.cs #	modified:   appsettings.json #

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var conn = builder.Configuration.GetConnectionString("DefaultConnection");
+//User ID=postgres;Password=1234;Server=192.168.68.210;Port=5432;Database=SampleDriver;IntegratedSecurity=true;Pooling=true;
 
 builder.Services.AddDbContext<ApiDbContext>(options => options.UseNpgsql(conn));
 builder.Services.AddControllers();
